095d3e3ac79a4c4585d4aaaac5abebb57769e8ab
Alex Rykov <alex.rykov@gmail.com>
2011-06-12 22:40:54 -0400

    scheduler modified

--- src/main/java/com/ryaltech/whitewater/gauges/services/GoogleScheduler.java
+++ src/main/java/com/ryaltech/whitewater/gauges/services/GoogleScheduler.java
@@ -1,5 +1,7 @@
 package com.ryaltech.whitewater.gauges.services;
 
+
+import com.google.appengine.api.taskqueue.RetryOptions;
 import com.google.appengine.api.taskqueue.TaskOptions;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
@@ -9,8 +11,11 @@ public class GoogleScheduler implements Scheduler {
 	@Override
 	public void scheduleRiverLevelUpdate(RiverInfo ri) {
 		Queue q = QueueFactory.getQueue("scheduleQueue");
+		
+		
+		
 		q.add(TaskOptions.Builder.withUrl(String.format(
-				"/services/updateRiverLevel/%s", ri.getRiverId())));
+				"/services/updateRiverLevel/%s", ri.getRiverId()))/*.method(TaskOptions.Method.GET)*/);
 
 	}
 
